b82ef40ea89d49a2be12310476a8406000c78264
tejaspavanb <tejaspavanb@gmail.com>
2025-11-12 18:14:49 +0530

    update

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,9 +1,15 @@
-import { generateComponents } from "@uploadthing/react";
+import { generateUploadButton, generateUploadDropzone, generateUploader } from "@uploadthing/react";
 
 import type { AppFileRouter } from "@/app/api/uploadthing/core";
 
-export const { UploadButton, UploadDropzone, Uploader } =
- generateComponents<AppFileRouter>({
-  url: typeof window !== "undefined" ? `${window.location.origin}/api/uploadthing` : undefined,
- });
+const clientConfig =
+ typeof window !== "undefined"
+  ? {
+     url: `${window.location.origin}/api/uploadthing`,
+    }
+  : undefined;
+
+export const UploadButton = generateUploadButton<AppFileRouter>(clientConfig);
+export const UploadDropzone = generateUploadDropzone<AppFileRouter>(clientConfig);
+export const Uploader = generateUploader<AppFileRouter>(clientConfig);
 
